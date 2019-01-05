7df532983cc3757935da70376245cfe3fd766f9b
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-01-05 01:05:07 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram# Your code goes here!
+attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+def match(arrayofwords)
+  arrayofwords.select do |element|
+  (@word.split("").sort) == (element.split("").sort)
+
+end
+
+end
+end
