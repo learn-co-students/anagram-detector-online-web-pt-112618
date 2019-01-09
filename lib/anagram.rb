311daf918571c6bce1ca3251bbec7d9e5530faaf
Wendi Chen <epanistamai.proles@gmail.com>
2019-01-09 22:58:06 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end  
+  
+  def match(word_arr)
+    new_arr = @word.split("").sort
+    word_arr.select do |word|
+      new_word = word.split("").sort
+        if new_word == new_arr
+          word
+        else
+          nil
+        end
+    end
+    
+  end
+  
+  
+  
+  
+end
